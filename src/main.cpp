463acd96bfa6c031bdfd984fac8b5313083061da
DESKTOP-18CC8V4\Daniel Álvarez <daniel10alra@gmail.com>
2021-09-13 16:16:56 -0600

    Ultimas modificaciones al main

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,29 @@ using namespace std;
 
 int main() {
 
-    ServicioPostal*servicio1 = new ServicioPostal(6,6,1);
-    EnvioFedEX*fed1 = new EnvioFedEX(550, 6);
-    ServicioPostal*servicio2 = new ServicioPostal(10,9,2);
-    EnvioFedEX*fed2 = new EnvioFedEX(600,2);
+    ServicioPostal*servicioPostal1 = new ServicioPostal(6,6,1);
+    EnvioFedEX*envioFed1 = new EnvioFedEX(550, 6);
+    ServicioPostal*servicioPostal2 = new ServicioPostal(10,9,2);
+    EnvioFedEX*envioFed2 = new EnvioFedEX(600,2);
 
     std::vector<Envio * >arregloEnvios;
-    arregloEnvios.push_back(servicio1);
-    arregloEnvios.push_back(fed1);
-    arregloEnvios.push_back(servicio2);
-    arregloEnvios.push_back(fed2);
+    arregloEnvios.push_back(servicioPostal1);
+    arregloEnvios.push_back(envioFed1);
+    arregloEnvios.push_back(servicioPostal2);
+    arregloEnvios.push_back(envioFed2);
 
-    float total=0;
+    double totalMontoEnvios=0;
 
     for(Envio*envio:arregloEnvios){
-        total += envio->CalculoEnvio();
+        totalMontoEnvios += envio->CalculoEnvio();
     }
 
-    std::cout << "El total de los envios es de " << total << " colones" << std::endl;
+    std::cout << "El total de los envios es de " << totalMontoEnvios << " colones" << std::endl;
 
-    delete servicio1;
-    delete servicio2;
-    delete fed1;
-    delete fed2;
+    delete servicioPostal1;
+    delete servicioPostal2;
+    delete envioFed1;
+    delete envioFed2;
 
     return 0;
 }
